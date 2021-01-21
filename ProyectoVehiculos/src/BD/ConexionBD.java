c2334266d0da07cae6771a3795666b4844bc10ff
T97 <56069533+TheAragont97@users.noreply.github.com>
2021-01-21 09:16:30 +0100

    proyectoVehiculos

---
+++ ProyectoVehiculos/src/BD/ConexionBD.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BD;
+
+/**
+ *
+ * @author USUARIO
+ */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConexionBD {
+
+   
+  
+    static Connection conn=null;
+    static Statement st=null;
+    static ResultSet rs=null;
+    static String bd="clase";//cambiar
+    static String login="root";
+    static String password="";  
+    static String url="jdbc:mysql://localhost/"+bd;
+
+    
+    public static Connection enlace (Connection conn) 
+{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(url,login,password);
+        } catch (SQLException ex) {
+          System.out.println("Excepicon en la conexión");
+        } catch (ClassNotFoundException ex) {
+           System.out.println("No se encuentra la clase");
+        }
+    return conn;
+}
+    
+     static ResultSet consulta() {
+         conn=enlace(conn);
+        try {
+            st=conn.createStatement();
+        } catch (SQLException ex) {
+            System.out.println("excepcion");
+        }
+        try {
+            rs=st.executeQuery("select * from alumno");
+        } catch (SQLException ex) {
+           System.out.println("error en la query");
+        }
+        
+        return rs;
+    }
+
+
+     public static void imprimirConsulta(ResultSet rs){
+        try {
+            while (rs.next()){
+                System.out.print(rs.getInt(1)+"    ");
+                System.out.print(rs.getString("Nombre")+"     ");
+                System.out.print(rs.getString("Apellido1")+"     ");
+                System.out.println(rs.getString("Apellido2"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error en el resultset");
+            ex.printStackTrace();
+        }
+     }
+     
+     public static void cerrarSesion() {
+        try {
+            rs.close();
+            st.close();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     }
+    public static void main(String[] args) {
+        
+        
+        conn=enlace(conn);
+        rs=consulta();
+        imprimirConsulta(rs);
+        cerrarSesion();
+        
+    }
+}
---
+++ ProyectoVehiculos/src/POJOs/Vehiculo.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POJOs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Vehiculo {
+    //Atributos
+    private String matricula;
+    private String marca;
+    private String modelo;
+
+    public Vehiculo(String matricula, String marca, String modelo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
